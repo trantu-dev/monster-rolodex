52e2b4b30ae7e27a46f630a73a72dd5d5c3d246c
trantu-dev <trananhtu@protonmail.ch>
2020-02-13 17:17:44 +0700

    Updates

---
+++ precache-manifest.6f9c1b7440c667ab7c1cbde00c2a96e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f5310f593878c78e1bc797a069e633",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6e81cdbda490de465354",
+    "url": "/monster-rolodex/static/css/main.1ff78b1a.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e81cdbda490de465354",
+    "url": "/monster-rolodex/static/js/main.64297c37.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
